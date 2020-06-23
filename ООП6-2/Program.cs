9bf4b722f8e73780ef4343690508c4d1055aca48
danil <danil@DESKTOP-B5464OT>
2020-06-23 11:35:02 +0500

    Добавьте файлы проекта.

---
+++ ООП6-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП6_2
+{
+    class Class
+    {
+        private string Metod;
+
+        public Class()
+        {
+            Metod = "Пусто";
+        }
+        public Class(string Line)
+        {
+            if (Line.Length > 10)
+                this.Metod = "Слишком длинная строка";
+            else
+                this.Metod = Line;
+        }
+        public void Display()
+        {
+            Console.WriteLine(Metod);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class classone = new Class();
+            classone.Display();
+            Class classtwo = new Class("1234567890");
+            classtwo.Display();
+            Class classthre = new Class("2200-0032-1265-5342");
+            classthre.Display();
+            Console.ReadKey();
+        }
+    }
+}
